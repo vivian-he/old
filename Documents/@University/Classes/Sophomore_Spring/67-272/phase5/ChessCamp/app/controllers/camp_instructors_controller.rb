63f657e6f546a91c89f2a5a2b0328aa6fc27a7d3
Vivian <vhe1@andrew.cmu.edu>
2018-04-27 20:47:25 -0400

    initial commit

---
+++ Documents/@University/Classes/Sophomore_Spring/67-272/phase5/ChessCamp/app/controllers/camp_instructors_controller.rb
@@ -0,0 +1,37 @@
+class CampInstructorsController < ApplicationController
+  
+  def new
+    @camp_instructor   = CampInstructor.new
+    @camp              = Camp.find(params[:camp_id])
+    @other_instructors = @camp.instructors
+  end
+  
+  def create
+    @camp_instructor = CampInstructor.new(camp_instructor_params)
+    if @camp_instructor.save
+      flash[:notice] = "Successfully added instructor."
+      redirect_to camp_path(@camp_instructor.camp)
+    else
+      @camp = Camp.find(params[:camp_instructor][:camp_id])
+      @other_instructors = @camp.instructors
+      render action: 'new', locals: { camp: @camp, other_instructors: @other_instructors }
+    end
+  end
+ 
+  def destroy
+    # @camp_instructor = CampInstructor.find(params[:id])
+    camp_id = params[:id]
+    instructor_id = params[:instructor_id]
+    @camp_instructor = CampInstructor.where(camp_id: camp_id, instructor_id: instructor_id).first
+    unless @camp_instructor.nil?
+      @camp_instructor.destroy
+      flash[:notice] = "Successfully removed this instructor."
+    end
+  end
+
+  private
+    def camp_instructor_params
+      params.require(:camp_instructor).permit(:camp_id, :instructor_id)
+    end
+
+end
